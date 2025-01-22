4b36f9b46cfcad570ab890bf49766a2167e52e2c
Gilles Celli <gilles.celli@ecgs.lu>
2025-01-22 17:04:52 +0100

    macOS - Simplify NumPy finding
    On macOS we use Python3.12 and later Virtual Environment to install NumPy

--- cmake/FindNumpy.cmake
+++ cmake/FindNumpy.cmake
@@ -4,19 +4,22 @@
 # PYTHON_NUMPY_FOUND        - system has numarray development package and it should be used
 # PYTHON_NUMPY_INCLUDE_DIR  - directory where the arrayobject.h header file can be found
 
+IF(NOT APPLE)
+# On macOS we use cmake's integrated NumPy package finder and/or Numpy installed in a Python3 Virtual Environment
 IF(PYTHON_EXECUTABLE)
 	FILE(WRITE ${CMAKE_CURRENT_BINARY_DIR}/det_npp.py "from __future__ import print_function\ntry: import numpy; print(numpy.get_include())\nexcept: pass\n")
-	EXEC_PROGRAM("${PYTHON_EXECUTABLE}"
-		ARGS "\"${CMAKE_CURRENT_BINARY_DIR}/det_npp.py\""
-		OUTPUT_VARIABLE NUMPY_PATH
-	)
-	FILE(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/det_npp.py)
+	    EXEC_PROGRAM("${PYTHON_EXECUTABLE}"
+		    ARGS "\"${CMAKE_CURRENT_BINARY_DIR}/det_npp.py\""
+		    OUTPUT_VARIABLE NUMPY_PATH
+	    )
+    	FILE(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/det_npp.py)
 ENDIF(PYTHON_EXECUTABLE)
 
 FIND_PATH(PYTHON_NUMPY_INCLUDE_DIR numpy/arrayobject.h
 	"${NUMPY_PATH}/"
 	DOC "Directory where the numpy/arrayobject.h header file can be found. This file is part of the numpy package"
 )
+ENDIF(NOT APPLE)
 
 IF(PYTHON_NUMPY_INCLUDE_DIR)
 	SET (PYTHON_NUMPY_FOUND 1 CACHE INTERNAL "Python numpy development package is available")
