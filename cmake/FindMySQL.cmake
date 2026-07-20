ee3bd8e10280222bb276bc529b9169c23fbf9cda
Gilles Celli <gilles.celli@ecgs.lu>
2026-07-20 15:01:24 +0200

    macOS - Update MariaDB cmake for Homebrew
    Better support of MariaDB for Homebrew

--- cmake/FindMySQL.cmake
+++ cmake/FindMySQL.cmake
@@ -13,49 +13,56 @@
 # Modified by Jan Becker, <jabe@gfz-potsdam.de>
 #  * added REQUIRED and QUIETLY check
 #  * search for mysql/mysql.h instead of just mysql.h 
+
 IF(APPLE)
-    EXECUTE_PROCESS(COMMAND brew --prefix mysql
-	  RESULT_VARIABLE BREW_MYSQL
-	  OUTPUT_VARIABLE BREW_MYSQL_PREFIX
-	  OUTPUT_STRIP_TRAILING_WHITESPACE
+     # Find Homebrew version of MariaDB (an open-source drop-in replacement of MySQL)
+	EXECUTE_PROCESS(COMMAND brew --prefix mariadb
+	    RESULT_VARIABLE BREW_MARIADB_RESULT
+	    OUTPUT_VARIABLE BREW_MARIADB_PREFIX
+	    OUTPUT_STRIP_TRAILING_WHITESPACE
 	)
-    
-    IF(BREW_MYSQL EQUAL 0 AND EXISTS "${BREW_MYSQL_PREFIX}")
-	    MESSAGE(STATUS "Found MySQL installed by Homebrew at ${BREW_MYSQL_PREFIX}")
-	    SET(MySQL_DIR ${BREW_MYSQL_PREFIX})
-	    SET(MYSQL_INCLUDE_DIR ${BREW_MYSQL_PREFIX}/include)
-	    SET(MYSQL_LIBRARIES ${BREW_MYSQL_PREFIX}/lib/libmysqlclient.dylib)
-	ELSE()
-    	# macOS: Some user(s) prefer MariaDB over MySQL
-        # find Homebrew version of MariaDB (an open-source drop-in replacement of MySQL)
-	    EXECUTE_PROCESS(COMMAND brew --prefix mariadb
-	        RESULT_VARIABLE BREW_MARIADB
-	        OUTPUT_VARIABLE BREW_MARIADB_PREFIX
-	        OUTPUT_STRIP_TRAILING_WHITESPACE
-	    )
 
-	    IF(BREW_MARIADB EQUAL 0 AND EXISTS "${BREW_MARIADB_PREFIX}")
-            MESSAGE(STATUS "Found MariaDB/MySQL installed by Homebrew at ${BREW_MYSQL_PREFIX}")
-	        SET(MySQL_DIR ${BREW_MARIADB_PREFIX})
-	        SET(MYSQL_INCLUDE_DIR ${BREW_MARIADB_PREFIX}/include)
-	        SET(MYSQL_LIBRARIES ${BREW_MARIADB_PREFIX}/lib/libmysqlclient.dylib)    
+	IF(BREW_MARIADB_RESULT EQUAL 0 AND EXISTS "${BREW_MARIADB_PREFIX}")
+        MESSAGE(STATUS "Found MariaDB/MySQL installed by Homebrew at ${BREW_MYSQL_PREFIX}")
+	    
+        FIND_PATH(MYSQL_INCLUDE_DIR 
+            NAMES mysql.h mariadb_version.h
+            PATHS "${BREW_MARIADB_PREFIX}/include/mysql" "${BREW_MARIADB_PREFIX}/include/mariadb"
+            NO_DEFAULT_PATH
+        )
+
+        FIND_LIBRARY(MYSQL_LIBRARY 
+            NAMES mariadb mariadbclient mysqlclient
+            PATHS "${BREW_MARIADB_PREFIX}/lib"
+            NO_DEFAULT_PATH
+        )
+        
+        IF(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
+            SET(MYSQL_LIBRARIES ${MYSQL_LIBRARY})
+            SET(MYSQL_FOUND TRUE)
+            MESSAGE(STATUS "Homebrew MariaDB Headers: ${MYSQL_INCLUDE_DIR}")
+            MESSAGE(STATUS "Homebrew MariaDB Library: ${MYSQL_LIBRARIES}")
         ELSE()
-            MESSAGE(FATAL_ERROR "Homebrew MySQL/MariaDB not found! Either install MariaDB OR MySQL, but NOT both together with command: brew install mysql OR brew install mariadb")
+            MESSAGE(WARNING "MariaDB found by brew, but headers or libraries are missing inside the prefix.")
         ENDIF()
+    ELSE()
+        MESSAGE(FATAL_ERROR "Homebrew version of Mariadb not found. Install with: brew install mariadb")
     ENDIF()
-ENDIF(APPLE)
-				
-if(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
-   set(MYSQL_FOUND TRUE)
+        
+ENDIF()
 
-else(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
+IF(NOT APPLE)			
+    if(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
+        set(MYSQL_FOUND TRUE)
 
-  find_path(MYSQL_INCLUDE_DIR mysql/mysql.h
+    else(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
+
+    find_path(MYSQL_INCLUDE_DIR mysql/mysql.h
       $ENV{ProgramFiles}/MySQL/*/include
       $ENV{SystemDrive}/MySQL/*/include
       )
 
-  find_library(MYSQL_LIBRARIES NAMES mysqlclient
+    find_library(MYSQL_LIBRARIES NAMES mysqlclient
       PATHS
       /usr/lib/mysql
       /usr/local/lib/mysql
@@ -63,22 +70,23 @@ else(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
       $ENV{SystemDrive}/MySQL/*/include
       )
 
-  if(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
-    set(MYSQL_FOUND TRUE)
-    if(NOT MySQL_FIND_QUIETLY)
-	message(STATUS "Found MySQL: ${MYSQL_INCLUDE_DIR}, ${MYSQL_LIBRARIES}")
-    endif(NOT MySQL_FIND_QUIETLY)
-  else(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
-    set(MYSQL_FOUND FALSE)
-    if(MySQL_FIND_REQUIRED)
-	message(FATAL_ERROR "MySQL not found.")
-    else(MySQL_FIND_REQUIRED)
+    if(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
+        set(MYSQL_FOUND TRUE)
+        if(NOT MySQL_FIND_QUIETLY)
+	        message(STATUS "Found MySQL: ${MYSQL_INCLUDE_DIR}, ${MYSQL_LIBRARIES}")
+        endif(NOT MySQL_FIND_QUIETLY)
+    else(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
+        set(MYSQL_FOUND FALSE)
+        if(MySQL_FIND_REQUIRED)
+	    message(FATAL_ERROR "MySQL not found.")
+        else(MySQL_FIND_REQUIRED)
 	if(NOT MySQL_FIND_QUIETLY)
 	    message(STATUS "MySQL not found.")
 	endif(NOT MySQL_FIND_QUIETLY)
     endif(MySQL_FIND_REQUIRED)
-  endif(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
+    endif(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
 
-  mark_as_advanced(MYSQL_INCLUDE_DIR MYSQL_LIBRARIES)
+    mark_as_advanced(MYSQL_INCLUDE_DIR MYSQL_LIBRARIES)
 
 endif(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
+ENDIF()
