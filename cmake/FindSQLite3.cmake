f10f4b71adb7d833f3958fb5f1eb719459c7b071
Gilles Celli <gilles.celli@ecgs.lu>
2026-07-24 15:31:41 +0200

    macOS - better detect defautl system SQLite3
    Use macOS default system SQLite3 since Homebrew SQLite3 is keg only

--- cmake/FindSQLite3.cmake
+++ cmake/FindSQLite3.cmake
@@ -14,21 +14,25 @@
 #  * added REQUIRED and QUIETLY check
 
 IF(APPLE)
-		EXECUTE_PROCESS(COMMAND brew --prefix sqlite
-			RESULT_VARIABLE BREW_SQLITE3
-			OUTPUT_VARIABLE BREW_SQLITE3_PREFIX
-			OUTPUT_STRIP_TRAILING_WHITESPACE
-		)
-		IF(BREW_SQLITE3 EQUAL 0 AND EXISTS "${BREW_SQLITE3_PREFIX}")
-			MESSAGE(STATUS "Found SQLITE3 installed by Homebrew at ${BREW_SQLITE3_PREFIX}")
-			SET(SQLITE3_EXECUTABLE ${BREW_SQLITE3_PREFIX}/bin/SQLITE3)
-			SET(SQLITE3_INCLUDE_DIR ${BREW_SQLITE3_PREFIX}/include/)
-			INCLUDE_DIRECTORIES(${SQLITE3_INCLUDE_DIR})
-		ELSE()
-			MESSAGE(FATAL_ERROR "Homebrew version of SQLite3 not found! Install with: brew install sqlite")
-		ENDIF()
+    # Use cmake's FindSQLite3 module 
+    FIND_PACKAGE(SQLite3 REQUIRED)
+    
+    # On macOS we use cmake's internal FindSQLite3 (since cmake v3.14)
+	# Note cmake's results name are: SQLite3_INCLUDE_DIRS and SQLite3_LIBRARIES
+	# but SeisComP uses uppercase: SQLITE3_INCLUDE_DIR & SQLITE3_LIBRARIES so we need to set this when found.
+	IF(SQLite3_FOUND)
+	    MESSAGE(STATUS "macOS SQLite3 found!")
+	    MESSAGE(STATUS "macOS SQLite3_INCLUDE_DIRS: ${SQLite3_INCLUDE_DIRS} ")
+	    MESSAGE(STATUS "macOS SQLite3_LIBRARIES: ${SQLite3_LIBRARIES} ")
+	    SET(SQLITE3_INCLUDE_DIR ${SQLite3_INCLUDE_DIRS})
+	    SET(SQLITE3_LIBRARIES ${SQLite3_LIBRARIES})
+	ELSE()
+	    MESSAGE(STATUS "macOS SQLite not found! macOS has its own SQLite3 version installed when Xcode or CommandLineTools are installed. Either install Xcode.app or CommandLineTools ")    
+	    MESSAGE(FATAL_ERROR "or as analternative install sqlite3 with Homebrew: brew install sqlite")    
+	ENDIF()
 ENDIF(APPLE)
 
+IF(NOT APPLE)
 if(SQLITE3_INCLUDE_DIR AND SQLITE3_LIBRARIES)
    set(SQLITE3_FOUND TRUE)
 
@@ -57,3 +61,4 @@ else(SQLITE3_INCLUDE_DIR AND SQLITE3_LIBRARIES)
   mark_as_advanced(SQLITE3_INCLUDE_DIR SQLITE3_LIBRARIES)
 
 endif(SQLITE3_INCLUDE_DIR AND SQLITE3_LIBRARIES)
+ENDIF(NOT APPLE)
