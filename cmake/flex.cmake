0ed542fe568619d9efca096f33a74b4cbc7deb38
Gilles Celli <gilles.celli@ecgs.lu>
2026-07-24 15:17:48 +0200

    macOS - better detection of Flex and set as a macro
    Previous version was not inside the Macro function FIND_FLEX

--- cmake/flex.cmake
+++ cmake/flex.cmake
@@ -1,44 +1,47 @@
 # flex a .l file
 
-IF(APPLE)
-    IF (EXISTS "/Library/Developer/CommandLineTools/usr/include/FlexLexer.h" )
-        SET(FLEX_EXECUTABLE /usr/bin/flex CACHE FILEPATH "Flex executable")
-        SET(FLEX_INCLUDE_DIR "/Library/Developer/CommandLineTools/usr/include/")
-        INCLUDE_DIRECTORIES(${FLEX_INCLUDE_DIR})
-        MESSAGE(STATUS "macOS default /usr/bin/flex found.")
-    ELSE()
-        MESSAGE(STATUS "macOS default /Library/Developer/CommandLineTools/usr/include/FlexLexer.h not found! Checking Homebrew version of flex.")
-		EXECUTE_PROCESS(COMMAND brew --prefix flex
-			RESULT_VARIABLE BREW_FLEX
-			OUTPUT_VARIABLE BREW_FLEX_PREFIX
-			OUTPUT_STRIP_TRAILING_WHITESPACE
-		)
-		IF(BREW_FLEX EQUAL 0 AND EXISTS "${BREW_FLEX_PREFIX}")
-			MESSAGE(STATUS "Found flex installed by Homebrew at ${BREW_FLEX_PREFIX}")
-			SET(FLEX_EXECUTABLE ${BREW_FLEX_PREFIX}/bin/flex CACHE FILEPATH "Flex executable")
-			SET(FLEX_INCLUDE_DIR ${BREW_FLEX_PREFIX}/include/)
-			INCLUDE_DIRECTORIES(${FLEX_INCLUDE_DIR})
-		ELSE()
-			MESSAGE(FATAL_ERROR "Homebrew version of flex not found! Install with: brew install flex")
-		ENDIF()
-	ENDIF()	
-ENDIF(APPLE)
-
 # search flex
 MACRO(FIND_FLEX)
-	IF(NOT FLEX_EXECUTABLE)
-		FIND_PROGRAM(FLEX_EXECUTABLE flex)
-		IF (NOT FLEX_EXECUTABLE)
-			MESSAGE(FATAL_ERROR "flex not found - aborting")
-		ENDIF (NOT FLEX_EXECUTABLE)
-	ENDIF(NOT FLEX_EXECUTABLE)
+    IF(APPLE)
+        IF (EXISTS "/Library/Developer/CommandLineTools/usr/include/FlexLexer.h" )
+            SET(FLEX_EXECUTABLE /usr/bin/flex CACHE FILEPATH "Flex executable")
+            SET(FLEX_INCLUDE_DIR "/Library/Developer/CommandLineTools/usr/include/")
+            INCLUDE_DIRECTORIES(${FLEX_INCLUDE_DIR})
+            MESSAGE(STATUS "macOS default /usr/bin/flex and header FlexLexer.h found.")
+        ELSE()
+            MESSAGE(STATUS "macOS default flex from Xcode or CommandLineTools not found! Checking Homebrew version of flex.")
+		    EXECUTE_PROCESS(COMMAND brew --prefix flex
+			    RESULT_VARIABLE BREW_FLEX
+			    OUTPUT_VARIABLE BREW_FLEX_PREFIX
+			    OUTPUT_STRIP_TRAILING_WHITESPACE
+		    )
+		    IF(BREW_FLEX EQUAL 0 AND EXISTS "${BREW_FLEX_PREFIX}")
+			    MESSAGE(STATUS "Found flex installed by Homebrew at ${BREW_FLEX_PREFIX}")
+			    SET(FLEX_EXECUTABLE ${BREW_FLEX_PREFIX}/bin/flex CACHE FILEPATH "Flex executable")
+			    SET(FLEX_INCLUDE_DIR ${BREW_FLEX_PREFIX}/include/)
+			    INCLUDE_DIRECTORIES(${FLEX_INCLUDE_DIR})
+		    ELSE()	    
+			    MESSAGE(FATAL_ERROR "Homebrew version of flex not found! Install with: brew install flex")
+		    ENDIF()
+	    ENDIF()	
+    ENDIF(APPLE)
 
-	IF(NOT FLEX_INCLUDE_DIR)
-		FIND_PATH(FLEX_INCLUDE_DIR FlexLexer.h)
-		IF (NOT FLEX_INCLUDE_DIR)
-			MESSAGE(FATAL_ERROR "FlexLexer.h not found - aborting")
-		ENDIF (NOT FLEX_INCLUDE_DIR)
-	ENDIF (NOT FLEX_INCLUDE_DIR)
+    IF(NOT APPLE)
+	    IF(NOT FLEX_EXECUTABLE)
+		    FIND_PROGRAM(FLEX_EXECUTABLE flex)
+		    IF (NOT FLEX_EXECUTABLE)
+			    MESSAGE(FATAL_ERROR "flex not found - aborting")
+		    ENDIF (NOT FLEX_EXECUTABLE)
+	    ENDIF(NOT FLEX_EXECUTABLE)
+
+	    IF(NOT FLEX_INCLUDE_DIR)
+		    FIND_PATH(FLEX_INCLUDE_DIR FlexLexer.h)
+		    IF (NOT FLEX_INCLUDE_DIR)
+		    	MESSAGE(FATAL_ERROR "FlexLexer.h not found - aborting")
+		    ENDIF (NOT FLEX_INCLUDE_DIR)
+	    ENDIF (NOT FLEX_INCLUDE_DIR)
+    ENDIF(NOT APPLE)
+    
 ENDMACRO(FIND_FLEX)
 
 MACRO(ADD_FLEX_FILES _sources)
